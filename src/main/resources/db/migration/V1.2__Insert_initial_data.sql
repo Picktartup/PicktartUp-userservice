489078713f8792e72fa0faa4d95de051878eb1c9
RyuChaeHyun <lch010201@gmail.com>
2024-11-22 18:00:43 +0900

    🐛fix: fly-away 디비 생성 에러

---
+++ src/main/resources/db/migration/V1.2__Insert_initial_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (name, email, encrypted_pwd, role, is_activated, created_at)
+VALUES
+    ('김관리', 'admin1@picktartup.com', '$2a$10$YOUR_ENCRYPTED_PWD_HERE', 'ADMIN', true, CURRENT_TIMESTAMP),
+    ('이어드민', 'admin2@picktartup.com', '$2a$10$YOUR_ENCRYPTED_PWD_HERE', 'ADMIN', true, CURRENT_TIMESTAMP),
+    ('박지민', 'jimin@gmail.com', '$2a$10$YOUR_ENCRYPTED_PWD_HERE', 'USER', true, CURRENT_TIMESTAMP),
+    ('김유진', 'yujin@naver.com', '$2a$10$YOUR_ENCRYPTED_PWD_HERE', 'USER', true, CURRENT_TIMESTAMP),
+    ('이서준', 'seojun@kakao.com', '$2a$10$YOUR_ENCRYPTED_PWD_HERE', 'USER', true, CURRENT_TIMESTAMP),
+    ('최민서', 'minseo@gmail.com', '$2a$10$YOUR_ENCRYPTED_PWD_HERE', 'USER', true, CURRENT_TIMESTAMP);
+
