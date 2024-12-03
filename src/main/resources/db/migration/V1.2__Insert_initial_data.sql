dbc5c3b9a645f35c60638fbf5ff0f567330f42f3
RyuChaeHyun <lch010201@gmail.com>
2024-12-03 18:22:27 +0900

    🐛fix: user db에 실제 데이터 추가

--- src/main/resources/db/migration/V1.2__Insert_initial_data.sql
+++ src/main/resources/db/migration/V1.2__Insert_initial_data.sql
@@ -5,5 +5,8 @@ VALUES
     ('박지민', 'jimin@gmail.com', '$2a$10$YOUR_ENCRYPTED_PWD_HERE', 'USER', true, CURRENT_TIMESTAMP),
     ('김유진', 'yujin@naver.com', '$2a$10$YOUR_ENCRYPTED_PWD_HERE', 'USER', true, CURRENT_TIMESTAMP),
     ('이서준', 'seojun@kakao.com', '$2a$10$YOUR_ENCRYPTED_PWD_HERE', 'USER', true, CURRENT_TIMESTAMP),
-    ('최민서', 'minseo@gmail.com', '$2a$10$YOUR_ENCRYPTED_PWD_HERE', 'USER', true, CURRENT_TIMESTAMP);
+    ('최민서', 'minseo@gmail.com', '$2a$10$YOUR_ENCRYPTED_PWD_HERE', 'USER', true, CURRENT_TIMESTAMP),
+    ('류채현', 'lch010201@gmail.com', '$2a$10$YOUR_ENCRYPTED_PWD_HERE', 'USER', true, CURRENT_TIMESTAMP),
+    ('최나영', 'dmddl282@gmail.com', '$2a$10$YOUR_ENCRYPTED_PWD_HERE', 'USER', true, CURRENT_TIMESTAMP),
+
 
